1506c652523ae15a2d34acb245c35d5a0a0c1b90
sachinthakur9614 <sachin.thakur@mca.christuniversity.in>
2020-02-17 07:03:37 +0530

    fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,23 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
+
+
+
+REST_FRAMEWORK  = {
+
+'DEFAULT_AUTHENTICATION_CLASSES':{
+    'rest_framework.authentication.TokenAuthentication'
+
+},
+'DEFAULT_PERMISSION_CLASSES':(
+
+
+'rest_framework.permission.IsAuthrnticated'
+    ),
+ }
+ 
 
 
 # Database
@@ -125,3 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
  
 
     path('', views.index)
-    
+
 ]
+
